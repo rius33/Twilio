c287bf4810ddd328a0c5681518c4a04076f9de45
Tim OBrien <tgfobrien@gmail.com>
2014-06-16 20:50:53 -0500

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Rez_Master:app
---
+++ Rez_Master.py
@@ -0,0 +1,10 @@
+__author__ = 'DT'
+
+ import os
+ from flask import Flask
+
+ app = Flask(__name__)
+
+ @app.route('/')
+ def hello():
+     return 'Hello World!'
