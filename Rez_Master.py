0aa7ebdefaa37f1a74e616aae21347295b3a006f
Tim OBrien <tgfobrien@gmail.com>
2014-06-20 19:52:42 -0500

    secondary

--- Rez_Master.py
+++ Rez_Master.py
@@ -8,10 +8,10 @@ from twilio import twiml
 from twilio.rest import TwilioRestClient
 
 
-ACCOUNT_SID = "ACd44d711fa3867cbb1d77184dc48a69b2"
-AUTH_TOKEN = "79f744af5e9e19fae4215c4a4b602flf"
-#ACCOUNT_SID = "ACbdc62d802802d2191bdf844bfd208461"
-#AUTH_TOKEN = "8238ad2b6e2bdd428391914b780fc5c4"
+#ACCOUNT_SID = "ACd44d711fa3867cbb1d77184dc48a69b2"
+#AUTH_TOKEN = "79f744af5e9e19fae4215c4a4b602flf"
+ACCOUNT_SID = "ACbdc62d802802d2191bdf844bfd208461"
+AUTH_TOKEN = "8238ad2b6e2bdd428391914b780fc5c4"
 T_NUM = '+18602375985'
 E_NUM = '+16304733343'
 DEBUG_DICTIONARY = []
@@ -42,17 +42,20 @@ def deb():
 def SMS(Numbers, Body):
     # SOMETHING IS BROKEN
     # Download the Python helper library from twilio.com/docs/python/install
-    for number in Numbers:
-        client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-        message = client.messages.create(
-            body=Body, #Body is Str of message NOT TWIML
-            to=number,
-            from_=E_NUM, #Twilio number
-        )
-        print message.sid
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    print ACCOUNT_SID, AUTH_TOKEN
+    message = client.messages.create(
+        to=Numbers[0],
+        from_=T_NUM, #Twilio number
+        body=Body, #Body is Str of message NOT TWIML
+    )
+    print message.sid
 
 
 def receive(From, To, Body):
     pass
 # a = ["+18604605536", "+19802970490", "+18603264336"]
 # SMS(a, "Yo this is Steve. Pumped to get in tomorrow let's throw a banger.")
+
+if __name__ == "__main__":
+    SMS(["+18603264336"],"fuck you")
