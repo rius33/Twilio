9efa4d54464be64ec170624edd31c04ffe3c9493
Tim OBrien <tgfobrien@gmail.com>
2014-06-24 20:15:06 -0500

    testing

--- Rez_Master.py
+++ Rez_Master.py
@@ -14,7 +14,7 @@ ACCOUNT_SID = "ACbdc62d802802d2191bdf844bfd208461"
 AUTH_TOKEN = "8238ad2b6e2bdd428391914b780fc5c4"
 T_NUM = '+18602375985'
 E_NUM = '+16304733343'
-#DEBUG_DICTIONARY = []
+DEBUG_DICTIONARY = []
 
 app = Flask(__name__)
 
@@ -26,12 +26,14 @@ def hello():
 def receive():
     from_num = request.values.get('From', None)
     incMessage = request.values.get('Body')
+    sms([from_num], incMessage)
+    call([from_num])
     #DEBUG_DICTIONARY.append(from_num)
     #DEBUG_DICTIONARY.append(incMessage)
     #resp = twilio.twiml.Response()
     #resp.message(incMessage)
     #DEBUG_DICTIONARY.append(str(resp))
-    sms([from_num], incMessage)
+    #sms([from_num], incMessage)
     #return str(resp)
 
 #@app.route('/Debug')
@@ -49,16 +51,19 @@ def sms(Numbers, Body):
     print message.sid
 
 
-def call(Numbers, Body):
-    pass
+def call(Numbers):
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    call = client.calls.create(to=Numbers[0],  # Any phone number
+                           from_=T_NUM, # Must be a valid Twilio number
+                           url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
 
 #def receive(From, To, Body):
  #   pass
 # a = ["+18604605536", "+19802970490", "+18603264336"]
 # SMS(a, "Yo this is Steve. Pumped to get in tomorrow let's throw a banger.")
 
-if __name__ == "__main__":
-    sms(["+18603264336"], "fuck you")
+#if __name__ == "__main__":
+#    call(["+18603264336"])
 
 
     ''' message.replace("<Say>", "<Response><Say>")
