4aef0da2e298ced0c451c95af594878c0f2cffd7
Tim OBrien <tgfobrien@gmail.com>
2014-06-25 21:40:05 -0500

    Secondary

--- Rez_Master.py
+++ Rez_Master.py
@@ -10,8 +10,8 @@ from twilio.rest import TwilioRestClient
 
 #ACCOUNT_SID = "ACd44d711fa3867cbb1d77184dc48a69b2"
 #AUTH_TOKEN = "79f744af5e9e19fae4215c4a4b602flf"
-ACCOUNT_SID = "ACbdc62d802802d2191bdf844bfd208461"
-AUTH_TOKEN = "8238ad2b6e2bdd428391914b780fc5c4"
+ACCOUNT_SID = os.environ['ACCOUNT_SID']
+AUTH_TOKEN = os.environ['AUTH_TOKEN']
 T_NUM = '+18602375985'
 E_NUM = '+16304733343'
 CALLERS = {
@@ -103,7 +103,8 @@ def call(Numbers):
                            from_=T_NUM, # Must be a valid Twilio number
                            url="http://obscure-savannah-9638.herokuapp.com/")
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    call(["+18603264336"])
 
 
     ''' message.replace("<Say>", "<Response><Say>")
