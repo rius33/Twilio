9010cbe55bac57cdcc0c841e550372f86b116b30
Tim OBrien <tgfobrien@gmail.com>
2014-06-29 16:50:12 -0500

    Secondary

--- Rez_Master.py
+++ Rez_Master.py
@@ -23,12 +23,12 @@ DEBUG_DICTIONARY = []
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def basic():
+@app.route('/')
+def hello():
     return "Hello World."
 
 @app.route("/call/<call_body>", methods=['GET', 'POST'])
-def hello(call_body):
+def basic(call_body):
     resp = twilio.twiml.Response()
     resp.say(call_body)
     return str(resp)
